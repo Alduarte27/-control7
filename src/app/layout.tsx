77facf3aa6605db7b7c9a052e6ec968779c7175b
Alexander Ayavaca Duarte <alexanderduarte2539@gmail.com>
2025-12-10 10:06:27 +0000

    NO VEO NINGUN CA,BIO, SABES QUE DEJA LA TIPOGRAFIA ORIGINAL QUE ESTABA A

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,11 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from '@/components/theme-provider';
-import { Poppins } from 'next/font/google';
 
 export const metadata: Metadata = {
   title: 'Control 7',
   description: 'Planificación y Seguimiento de la Producción',
 };
-
-const poppins = Poppins({ 
-  subsets: ['latin'], 
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-poppins' 
-});
 
 export default function RootLayout({
   children,
@@ -23,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
       <head />
-      <body className={`${poppins.variable} font-sans antialiased`}>
+      <body className={`antialiased`}>
         <ThemeProvider
             attribute="class"
             defaultTheme="system"
